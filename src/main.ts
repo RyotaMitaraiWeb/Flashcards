b8978500405c6ab88b036ebf58dfc2c71123ccc3
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-04-29 00:12:21 +0300

    adjust styling and icons

--- src/main.ts
+++ src/main.ts
@@ -10,17 +10,39 @@ import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 
+import { colors } from './constants/colors';
+import { getLocalStoragePalette, getLocalStorageTheme } from './util/localStorageTheme/localStoragePreferences';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
+import "@mdi/font/css/materialdesignicons.css";
+
+const app = createApp(App);
+app.use(createPinia());
+
+const palette = getLocalStoragePalette();
+
 const vuetify = createVuetify({
   components,
   directives,
+  theme: {
+    themes: {
+      light: {
+        dark: getLocalStorageTheme() === 'dark',
+        colors: {
+          primary: colors[palette],
+        }
+      }
+    }
+  },
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    }
+  }
 });
 
-import './assets/main.scss';
-
-const app = createApp(App);
-
 app.use(vuetify);
-app.use(createPinia());
 app.use(router);
 
 app.mount('#app');
