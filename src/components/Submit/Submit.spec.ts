dcb0c80270e4abcff00a5a0a48c547c3596f9957
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-05-01 17:27:50 +0300

    add Submit component

---
+++ src/components/Submit/Submit.spec.ts
@@ -0,0 +1,89 @@
+import { beforeEach, describe, expect, it } from 'vitest';
+import { render, screen } from '@testing-library/vue';
+
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import { createTestingPinia } from "@pinia/testing";
+import { useLoadingStore } from '../../stores/loading/loading';
+import Submit from './Submit.vue';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+describe('Submit component', () => {
+  it('Renders correctly', () => {
+    const pinia = createTestingPinia();
+
+    const { container } = render(Submit, {
+      global: {
+        plugins: [vuetify, pinia],
+      },
+      slots: {
+        default: 'Login',
+      },
+      props: {
+        icon: 'mdi-login-variant',
+      }
+    });
+
+    screen.findByText('Login');
+    const button = container.querySelector('.submit-btn') as HTMLButtonElement;
+
+    expect(button.disabled).toBe(false);
+
+    const icon = container.querySelector('.mdi-login-variant');
+    expect(icon).not.toBeNull();
+  });
+
+  it('Button is disabled when the global loading state has status true', () => {
+    const pinia = createTestingPinia({
+      stubActions: false,
+    });
+
+    const store = useLoadingStore();
+    store.startLoading();
+
+    const { container } = render(Submit, {
+      global: {
+        plugins: [vuetify, pinia],
+      },
+      slots: {
+        default: 'Login',
+      },
+      props: {
+        icon: 'mdi-login-variant',
+        color: 'indigo'
+      }
+    });
+
+    const button = container.querySelector('.submit-btn') as HTMLButtonElement;
+    expect(button.disabled).toBe(true);
+  });
+
+  it('Button is disabled when explicitly passed this state', () => {
+    const pinia = createTestingPinia({
+      stubActions: false,
+    });
+
+    const store = useLoadingStore();
+
+    const { container } = render(Submit, {
+      global: {
+        plugins: [vuetify, pinia],
+      },
+      slots: {
+        default: 'Login',
+      },
+      props: {
+        icon: 'mdi-login-variant',
+        disabled: true,
+      }
+    });
+
+    const button = container.querySelector('.submit-btn') as HTMLButtonElement;
+    expect(button.disabled).toBe(true);
+  });
+});
---
+++ src/components/Submit/Submit.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useLoadingStore } from '../../stores/loading/loading';
+import { storeToRefs } from 'pinia';
+const loadingStore = useLoadingStore();
+const { loading } = storeToRefs(loadingStore);
+
+export interface ISubmit {
+  icon?: string;
+  color?: string;
+  disabled?: boolean;
+}
+
+const props = defineProps<ISubmit>()
+</script>
+
+<template>
+  <v-btn class="submit-btn" :disabled="loading.status || props.disabled" variant="flat" type="submit" size="x-large" :prepend-icon="props.icon"
+    :color="props.color || 'primary'">
+    <slot>Submit</slot>  
+  </v-btn>
+</template>
