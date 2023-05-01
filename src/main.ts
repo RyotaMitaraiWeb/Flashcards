9301ee3f34fa7bcdbd8dd2f53e66a7ab439fdee3
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-05-01 17:29:32 +0300

    add session check upon initial load

--- src/main.ts
+++ src/main.ts
@@ -14,9 +14,27 @@ import { colors } from './constants/colors';
 import { getLocalStoragePalette, getLocalStorageTheme } from './util/localStorageTheme/localStoragePreferences';
 import { aliases, mdi } from 'vuetify/iconsets/mdi';
 import "@mdi/font/css/materialdesignicons.css";
+import type { ICreatedSession } from './types/components/auth';
+import { api } from './constants/api';
+import { HttpStatus } from './constants/httpstatus';
+import { useUserStore } from './stores/user/user';
+import { post } from './util/request/request';
 
 const app = createApp(App);
 app.use(createPinia());
+
+const userStore = useUserStore();
+
+const { setUser, restartUser } = userStore;
+
+const { res, data } = await post<ICreatedSession>(api.endpoints.accounts.session);
+  if (res.status === HttpStatus.CREATED) {
+    const { user, token } = data!;
+    setUser(user);
+    localStorage.setItem('accessToken', token);
+  } else {
+    restartUser();
+  }
 
 const palette = getLocalStoragePalette();
 
