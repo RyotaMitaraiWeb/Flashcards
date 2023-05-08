66335c7bb60e6774a88953c1b017ddd442b340d7
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-05-08 13:25:11 +0300

    construct a page not found view

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+document.title = 'Page not found';
+</script>
+
+<template>
+  <section id="not-found">
+    <h1>Page not found!</h1>
+    <p>The page you are looking for does not exist!</p>
+    <p>Back to <RouterLink to="/">home page</RouterLink></p>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+#not-found {
+  margin-top: 30px;
+  text-align: center;
+  font-size: 20pt;
+
+  h1, p {
+    margin-bottom: 20px;
+  }
+}
+</style>
