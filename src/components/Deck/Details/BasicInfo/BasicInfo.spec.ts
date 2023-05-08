9e604c879382fd83e69c84ea6088722630744de8
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-05-08 13:21:56 +0300

    add BasicInfo

---
+++ src/components/Deck/Details/BasicInfo/BasicInfo.spec.ts
@@ -0,0 +1,68 @@
+import { describe, it } from 'vitest';
+import BasicInfoVue from './BasicInfo.vue';
+import type { IDeck } from '../../../../types/components/decks';
+import { render, screen } from '@testing-library/vue';
+
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import { toShortDate } from '../../../../util/toShortDate/toShortDate';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+const deck: IDeck = {
+  title: 'test deck',
+  description: 'some description',
+  bookmarked: false,
+  id: 1,
+  authorId: 1,
+  createdAt: '2023-05-03T08:58:28.830Z',
+  updatedAt: '2023-05-03T08:58:28.830Z',
+  flashcards: [{
+    front: 'a',
+    back: 'a',
+  }],
+};
+
+describe('BasicInfo component', () => {
+  it('Renders correctly (has never been updated)', async () => {
+    render(BasicInfoVue, {
+      props: {
+        deck
+      },
+      global: {
+        plugins: [vuetify]
+      }
+    });
+
+    const date = toShortDate(deck.createdAt)
+
+    await screen.findByText(deck.title);
+    await screen.findByText(deck.description);
+    await screen.findByText(new RegExp(`^Created on: ${date}$`));
+  });
+
+  it('Renders correctly (has been updated)', async () => {
+    const updatedDeck = { ...deck };
+    updatedDeck.updatedAt = '2023-06-03T08:58:28.830Z'
+    render(BasicInfoVue, {
+      props: {
+        deck: updatedDeck,
+      },
+      global: {
+        plugins: [vuetify]
+      }
+    });
+
+    const createdAt = toShortDate(updatedDeck.createdAt);
+    const updatedAt = toShortDate(updatedDeck.updatedAt);
+
+    await screen.findByText(deck.title);
+    await screen.findByText(deck.description);
+    await screen
+      .findByText(`Created on: ${createdAt}. Last updated on: ${updatedAt}`);
+  });
+});
---
+++ src/components/Deck/Details/BasicInfo/BasicInfo.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+import type { IDeck } from '../../../../types/components/decks';
+import { toShortDate } from '../../../../util/toShortDate/toShortDate';
+
+export interface DeckDetails {
+  deck: IDeck,
+};
+
+const props = defineProps<DeckDetails>();
+const createdAt = toShortDate(props.deck.createdAt);
+const updatedAt = toShortDate(props.deck.updatedAt);
+
+const hasBeenUpdated = props.deck.createdAt !== props.deck.updatedAt;
+
+const subtitle = hasBeenUpdated
+  ? `Created on: ${createdAt}. Last updated on: ${updatedAt}`
+  : `Created on: ${createdAt}`;
+</script>
+
+<template>
+  <v-card 
+    :title="deck.title"
+    id="deck-basic-info"
+    :subtitle="subtitle"
+    :text="deck.description"
+  >
+  </v-card>
+</template>
+
+<style scoped lang="scss">
+#deck-basic-info {
+  width: 700px;
+  height: 400px;
+  
+  @media screen and (max-width: 420px) {
+    width: 300px;
+    height: 300px;
+  }
+}
+</style>
