abe50c9ea50fd17fcda70388267014d5f058f8bf
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-05-14 00:30:49 +0300

    update types/decks.ts

--- src/types/components/decks.ts
+++ src/types/components/decks.ts
@@ -13,3 +13,37 @@ export interface IFlashcard {
   front: string;
   back: string;
 }
+
+/**
+ * ```typescript
+ * interface ICatalogueList {
+    decks: ICatalogueDeck[];
+    total: number;
+  }
+ * ```
+ */
+export interface ICatalogueList {
+  decks: ICatalogueDeck[];
+  total: number;
+}
+
+/**
+ * ```typescript
+ * interface ICatalogueDeck {
+    id: number;
+    title: string;
+    description: string;
+    authorId: number;
+    createdAt: string;
+    updatedAt: string;
+  }
+ * ```
+ */
+export interface ICatalogueDeck {
+  id: number;
+  title: string;
+  description: string;
+  authorId: number;
+  createdAt: string;
+  updatedAt: string;
+}
