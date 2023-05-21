75e6553d2a51b47053d58ba860c5ea004895082b
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-05-21 23:07:01 +0300

    add MobileFlashcardForm component

---
+++ src/components/Deck/Form/FlashcardForm/MobileFlashcardForm/MobileFlashcardForm.spec.ts
@@ -0,0 +1,333 @@
+import { describe, expect, it } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/vue';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import MobileFlashcardFormVue from './MobileFlashcardForm.vue';
+import type { IFlashcard } from '../../../../../types/components/decks';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+function deckSeed(n: number = 1, text = '') {
+  const flashcards: IFlashcard[] = [];
+
+  for (let i = 0; i < n; i++) {
+    flashcards[i] = {
+      front: text,
+      back: text,
+    }
+  }
+
+  return flashcards;
+}
+
+describe('MobileFlashcardForm component', () => {
+  describe('General', () => {
+    it('Renders correctly if no flashcard props are passed to it', async () => {
+      render(MobileFlashcardFormVue, {
+        global: {
+          plugins: [vuetify],
+        },
+      });
+
+      await screen.findByText('1 / 1');
+    });
+
+    it('Renders correctly if flashcard props are passed to it', async () => {
+      render(MobileFlashcardFormVue, {
+        global: {
+          plugins: [vuetify],
+        },
+        props: {
+          flashcards: deckSeed(5, 'flashcard test'),
+        }
+      });
+
+      await screen.findByText('1 / 5');
+      await screen.findAllByDisplayValue('flashcard test');
+    });
+
+    it('Switches between sides and maintains their state successfully', async () => {
+      render(MobileFlashcardFormVue, {
+        global: {
+          plugins: [vuetify],
+        },
+      });
+
+      const frontSide = document.querySelector('.flashcard-input-wrapper.front') as HTMLElement;
+      const backSide = document.querySelector('.flashcard-input-wrapper.back') as HTMLElement;
+
+      expect(backSide.style.display).toBe('none');
+
+      const frontTab = await screen.findByLabelText('Switch to front side');
+      const backTab = await screen.findByLabelText('Switch to back side');
+
+      const textarea = document.querySelector('textarea');
+
+      await fireEvent.update(textarea as Element, 'test');
+
+      await fireEvent.click(backTab);
+
+      expect(frontSide.style.display).toBe('none');
+
+      await fireEvent.click(frontTab);
+
+      expect(backSide.style.display).toBe('none');
+      expect(textarea?.value).toBe('test');
+    });
+  });
+
+  describe('Arrow buttons', () => {
+    it('Arrow buttons switch cards successfully', async () => {
+      render(MobileFlashcardFormVue, {
+        global: {
+          plugins: [vuetify],
+        },
+        props: {
+          flashcards: deckSeed(5),
+        },
+      });
+
+      const nextButton = await screen.findByLabelText('Move to next flashcard');
+      const previousButton = await screen.findByLabelText('Move to previous flashcard');
+
+      await fireEvent.click(nextButton);
+
+      await screen.findByText('2 / 5');
+
+      await fireEvent.click(previousButton);
+
+      await screen.findByText('1 / 5');
+    });
+
+    it('Right arrow button does not display when there is only one card', async () => {
+      render(MobileFlashcardFormVue, {
+        global: {
+          plugins: [vuetify],
+        },
+        props: {
+          flashcards: deckSeed(2),
+        }
+      });
+
+      const nextButton = await screen.findByLabelText('Move to next flashcard');
+      await fireEvent.click(nextButton);
+
+      expect(nextButton.classList.contains('invisible')).toBe(true);
+    });
+
+    it('Left arrow button does not display when there is only one card', async () => {
+      render(MobileFlashcardFormVue, {
+        global: {
+          plugins: [vuetify],
+        },
+      });
+
+      const previousButton = await screen.findByLabelText('Move to previous flashcard');
+      expect(previousButton.classList.contains('invisible')).toBe(true);
+    });
+
+    it('Both can be shown and hidden together', async () => {
+      render(MobileFlashcardFormVue, {
+        global: {
+          plugins: [vuetify],
+        },
+      });
+
+      const add = await screen.findByText('Add');
+
+      const nextButton = await screen.findByLabelText('Move to next flashcard');
+      const previousButton = await screen.findByLabelText('Move to previous flashcard');
+
+      expect(nextButton.classList.contains('invisible')).toBe(true);
+      expect(previousButton.classList.contains('invisible')).toBe(true);
+
+      await fireEvent.click(add);
+      await fireEvent.click(add);
+
+      await fireEvent.click(previousButton);
+
+      expect(nextButton.classList.contains('invisible')).toBe(false);
+      expect(previousButton.classList.contains('invisible')).toBe(false);
+    });
+
+    it('Left arrow switches to front side successfully', async () => {
+      render(MobileFlashcardFormVue, {
+        global: {
+          plugins: [vuetify],
+        },
+        props: {
+          flashcards: deckSeed(5),
+        }
+      });
+
+      const backSide = document.querySelector('.flashcard-input-wrapper.back') as HTMLElement;
+      const nextButton = await screen.findByLabelText('Move to next flashcard');
+      const previousButton = await screen.findByLabelText('Move to previous flashcard');
+      const backTab = await screen.findByLabelText('Switch to back side');
+
+      await fireEvent.click(nextButton);
+      await fireEvent.click(backTab);
+
+      await fireEvent.click(previousButton);
+
+      expect(backSide.style.display).toBe('none');
+    });
+
+    it('Right arrow switches to front side successfully', async () => {
+      render(MobileFlashcardFormVue, {
+        global: {
+          plugins: [vuetify],
+        },
+        props: {
+          flashcards: deckSeed(5),
+        }
+      });
+
+      const backSide = document.querySelector('.flashcard-input-wrapper.back') as HTMLElement;
+      const nextButton = await screen.findByLabelText('Move to next flashcard');
+      const backTab = await screen.findByLabelText('Switch to back side');
+
+      await fireEvent.click(backTab);
+
+      await fireEvent.click(nextButton);
+
+      expect(backSide.style.display).toBe('none');
+    });
+
+    it('Cards maintain their text even if switched', async () => {
+      render(MobileFlashcardFormVue, {
+        global: {
+          plugins: [vuetify],
+        },
+        props: {
+          flashcards: deckSeed(5),
+        }
+      });
+
+      const textarea = document.querySelector('textarea');
+      await fireEvent.update(textarea as HTMLElement, 'test');
+
+      const nextButton = await screen.findByLabelText('Move to next flashcard');
+      const previousButton = await screen.findByLabelText('Move to previous flashcard');
+
+      await fireEvent.click(nextButton);
+      await fireEvent.click(previousButton);
+
+      expect(textarea?.value).toBe('test');
+    });
+  });
+
+  describe('Add button', () => {
+    it('Add button works correctly', async () => {
+      render(MobileFlashcardFormVue, {
+        global: {
+          plugins: [vuetify],
+        },
+      });
+
+      const addButton = await screen.findByText('Add');
+      await fireEvent.click(addButton);
+
+      await screen.findByText('2 / 2');
+    });
+
+    it('Switches to front side', async () => {
+      render(MobileFlashcardFormVue, {
+        global: {
+          plugins: [vuetify],
+        },
+      });
+
+      const addButton = await screen.findByText('Add');
+      const backSide = document.querySelector('.flashcard-input-wrapper.back') as HTMLElement;
+
+      const backTab = await screen.findByLabelText('Switch to back side');
+
+      await fireEvent.click(backTab);
+      await fireEvent.click(addButton);
+      expect(backSide.style.display).toBe('none');
+    });
+  });
+
+  describe('Delete button', () => {
+    it('Delete button works correctly if the user is not on the first card', async () => {
+      render(MobileFlashcardFormVue, {
+        global: {
+          plugins: [vuetify],
+        },
+        props: {
+          flashcards: deckSeed(5),
+        },
+      });
+
+      const nextButton = await screen.findByLabelText('Move to next flashcard');
+
+      await fireEvent.click(nextButton);
+      await fireEvent.click(nextButton);
+
+      const removeButton = await screen.findByText('Delete');
+      await fireEvent.click(removeButton);
+
+      await screen.findByText('2 / 4');
+    });
+
+    it('Delete button works correctly if the user is on the first card', async () => {
+      render(MobileFlashcardFormVue, {
+        global: {
+          plugins: [vuetify],
+        },
+        props: {
+          flashcards: deckSeed(5),
+        },
+      });
+
+      const removeButton = await screen.findByText('Delete');
+      await fireEvent.click(removeButton);
+
+      await screen.findByText('1 / 4');
+    });
+
+    it('Delete button is not displayed if there is only one flashcard', async () => {
+      render(MobileFlashcardFormVue, {
+        global: {
+          plugins: [vuetify],
+        },
+      });
+
+      const invisibleDeleteButton = document.querySelector('.delete-btn') as HTMLElement;
+      expect(invisibleDeleteButton.style.display).toBe('none');
+
+      const addButton = await screen.findByText('Add');
+      await fireEvent.click(addButton);
+
+      const visibleDeleteButton = document.querySelector('.delete-btn') as HTMLElement;
+      expect(visibleDeleteButton.style.display).toBe('');
+    });
+
+    it('Switches to front side when clicked', async () => {
+      render(MobileFlashcardFormVue, {
+        global: {
+          plugins: [vuetify],
+        },
+        props: {
+          flashcards: deckSeed(5),
+        }
+      });
+
+      const deleteButton = await screen.findByText('Delete');
+      const backSide = document.querySelector('.flashcard-input-wrapper.back') as HTMLElement;
+
+      const backTab = await screen.findByLabelText('Switch to back side');
+
+      await fireEvent.click(backTab);
+
+      await fireEvent.click(deleteButton);
+
+      expect(backSide.style.display).toBe('none');
+    });
+  });
+});
---
+++ src/components/Deck/Form/FlashcardForm/MobileFlashcardForm/MobileFlashcardForm.vue
@@ -0,0 +1,162 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+import type { IFlashcard } from '../../../../../types/components/decks';
+import LeftArrow from '../../../../Flashcard/Arrows/LeftArrow/LeftArrow.vue';
+import RightArrow from '../../../../Flashcard/Arrows/RightArrow/RightArrow.vue';
+import FlashcardInput from '../FlashcardInput/FlashcardInput.vue';
+import { validationRules } from '../../../../../constants/validationRules';
+
+const tab = ref<'front' | 'back'>('front');
+export interface IFlashcardForm {
+  flashcards?: IFlashcard[];
+}
+
+const props = defineProps<IFlashcardForm>();
+const emit = defineEmits(['saveFlashcards']);
+
+const flashcards = ref(props.flashcards || [{
+  front: '',
+  back: '',
+}]);
+const index = ref(0);
+
+const frontSide = computed(() => flashcards.value[index.value].front);
+const backSide = computed(() => flashcards.value[index.value].back);
+
+function add() {
+  const flashcard: IFlashcard = {
+    front: '',
+    back: ''
+  };
+
+  flashcards.value.push(flashcard);
+  index.value = flashcards.value.length - 1;
+  tab.value = 'front';
+}
+
+function remove() {
+  const i = index.value;
+  flashcards.value.splice(i, 1);
+  if (i > 0) {
+    index.value--;
+  }
+
+  tab.value = 'front';
+}
+
+function moveToPreviousFlashcard() {
+  if (index.value > 0) {
+    index.value--;
+  }
+
+  tab.value = 'front';
+}
+
+function moveToNextFlashcard() {
+  if (index.value < flashcards.value.length - 1) {
+    index.value++;
+  }
+
+  tab.value = 'front';
+}
+
+function updateFlashcards(update: { side: 'front' | 'back', value: string }) {
+  const { value, side } = update;
+  flashcards.value[index.value][side] = value;
+  emit('saveFlashcards', flashcards.value);
+}
+
+
+</script>
+
+<template>
+  <section>
+    <div class="buttons">
+      <v-btn class="tab-btn" @click="tab = 'front'" color="primary" aria-label="Switch to front side">Front</v-btn>
+      <v-btn class="tab-btn" @click="tab = 'back'" color="primary" aria-label="Switch to back side">Back</v-btn>
+    </div>
+    <div class="controls">
+      <LeftArrow :class="index > 0 ? '' : 'invisible'" @click="moveToPreviousFlashcard"></LeftArrow>
+      <div class="cards">
+        <FlashcardInput v-show="tab === 'front'" :content="frontSide" side="front" @update-content="updateFlashcards">
+        </FlashcardInput>
+        <FlashcardInput v-show="tab === 'back'" :content="backSide" side="back" @update-content="updateFlashcards"></FlashcardInput>
+      </div>
+      <RightArrow :class="index < flashcards.length - 1 ? '' : 'invisible'" @click="moveToNextFlashcard"></RightArrow>
+    </div>
+    <div class="others">
+      <p class="count">{{ index + 1 }} / {{ flashcards.length }}</p>
+      <v-btn @click.prevent="add" class="add-btn flashcard-btn" color="green">
+        <template v-slot:prepend>
+          <v-icon icon="mdi-plus"></v-icon>
+        </template>
+        Add
+      </v-btn>
+      <v-btn @click.prevent="remove" class="delete-btn flashcard-btn" color="red-darken-4" v-show="flashcards.length > validationRules.deck.flashcards.minimumFlashcards">
+        <template v-slot:prepend>
+          <v-icon icon="mdi-delete"></v-icon>
+        </template>
+        Delete
+      </v-btn>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+section {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  gap: 20px;
+  margin-bottom: 50px;
+}
+
+.count {
+  font-size: 20pt;
+}
+
+.controls {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin-bottom: 80px;
+  @media screen and (max-width: 421px) {
+    flex-direction: column;
+
+    .cards {
+      margin-bottom: 4em;
+    }
+  }
+}
+
+.invisible {
+  visibility: hidden;
+}
+
+.others {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+  .flashcard-btn {
+    width: 300px;
+  }
+}
+
+.tab-btn {
+  width: 50%;
+  border-radius: 0;
+}
+
+.buttons {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  .flashcard-btn {
+    width: 100%;
+  }
+}
+</style>
