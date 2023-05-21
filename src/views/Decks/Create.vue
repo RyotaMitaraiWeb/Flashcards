213a9d42fdc6059bf85127e54acc6ae3ccb51e4a
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-05-21 23:34:06 +0300

    add a document title for create page

--- src/views/Decks/Create.vue
+++ src/views/Decks/Create.vue
@@ -1,7 +1,7 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
 import DeckForm from '../../components/Deck/Form/DeckForm.vue';
-
+document.title = 'Create a new deck';
 </script>
 
 <template>
