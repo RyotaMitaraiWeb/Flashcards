b8123100c72bae6bad31b630e7dbaf8d21bbecec
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-05-24 12:05:29 +0300

    add tests for delete button

--- e2e/decks.details.spec.ts
+++ e2e/decks.details.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
 import { api } from './util/api';
-import { rejectRequest } from './util/userAuthorization';
+import { authorizeRequest, rejectRequest } from './util/userAuthorization';
 import { HttpStatus } from '../src/constants/httpstatus';
-import { deckSeed } from './util/deck-seeds';
+import { deckCatalogueSeed, deckSeed } from './util/deck-seeds';
 
 const client = 'http://localhost:5173';
 const homePage = `${client}/`;
@@ -105,5 +105,179 @@ test.describe('Deck details page', () => {
 
     await page.goto(detailsPage);
     await page.waitForURL(homePage);
+  });
+
+  test.describe('Delete button', () => {
+    test.beforeEach(async ({ page }) => {
+      await page.route(detailsEndpoint, async (route) => {
+        if (route.request().method() !== 'GET') {
+          await route.fallback();
+          return;
+        }
+
+        await route.fulfill(deckSeed());        
+      });
+    });
+
+    test('Deleting from this page redirects to home when the server returns 403', async ({ page }) => {
+      await page.route(sessionEndpoint, async (route) => {
+        await route.fulfill(authorizeRequest());
+      });
+
+      await page.route(api.endpoints.decks.delete(1), async (route) => {
+        if (route.request().method() !== 'DELETE') {
+          await route.fallback();
+          return;
+        }
+        
+        await route.fulfill({
+          status: HttpStatus.FORBIDDEN,
+          contentType: 'application/json',
+          body: JSON.stringify({
+            message: ['You are not the creator of the deck'],
+            error: 'Forbidden',
+            statusCode: HttpStatus.FORBIDDEN,
+          }),
+        });
+      });
+
+      await page.route(api.endpoints.bookmarks.saved, async (route) => {
+        await route.fulfill(deckCatalogueSeed(12, 6));
+      });
+
+      await page.goto(detailsPage);
+
+      const del = page.getByText(/Delete/i);
+      
+      await del.click();
+
+      await page.waitForURL(homePage);
+    });
+
+    test('Deleting from this page redirects to home when successful', async ({ page }) => {
+      await page.route(sessionEndpoint, async (route) => {
+        await route.fulfill(authorizeRequest());
+      });
+
+      await page.route(api.endpoints.decks.delete(1), async (route) => {
+        if (route.request().method() !== 'DELETE') {
+          await route.fallback();
+          return;
+        }
+        
+        await route.fulfill({
+          status: HttpStatus.NO_CONTENT,
+        });
+      });
+
+      await page.route(api.endpoints.bookmarks.saved, async (route) => {
+        await route.fulfill(deckCatalogueSeed(12, 6));
+      });
+
+      await page.goto(detailsPage);
+
+      const del = page.getByText(/Delete/i);
+      
+      await del.click();
+
+      await page.waitForURL(homePage);
+    });
+
+    test('Deleting from this page redirects to not found page when the server returns 404', async ({ page }) => {
+      await page.route(sessionEndpoint, async (route) => {
+        await route.fulfill(authorizeRequest());
+      });
+
+      await page.route(api.endpoints.decks.delete(1), async (route) => {
+        if (route.request().method() !== 'DELETE') {
+          await route.fallback();
+          return;
+        }
+        
+        await route.fulfill({
+          status: HttpStatus.NOT_FOUND,
+          contentType: 'application/json',
+          body: JSON.stringify({
+            message: ['Deck does not exist'],
+            error: 'Not Found',
+            statusCode: HttpStatus.NOT_FOUND,
+          }),
+        });
+      });
+
+      await page.route(api.endpoints.bookmarks.saved, async (route) => {
+        await route.fulfill(deckCatalogueSeed(12, 6));
+      });
+
+      await page.goto(detailsPage);
+
+      const del = page.getByText(/Delete/i);
+      
+      await del.click();
+
+      await page.waitForURL(notFoundPage);
+    });
+
+    test('Deleting from this page redirects to home page when the server returns a non-standard error', async ({ page }) => {
+      await page.route(sessionEndpoint, async (route) => {
+        await route.fulfill(authorizeRequest());
+      });
+
+      await page.route(api.endpoints.decks.delete(1), async (route) => {
+        if (route.request().method() !== 'DELETE') {
+          await route.fallback();
+          return;
+        }
+        
+        await route.fulfill({
+          status: 500,
+          contentType: 'application/json',
+          body: JSON.stringify({
+            message: ['Server is down'],
+            error: 'Internal Server Error',
+            statusCode: 500,
+          }),
+        });
+      });
+
+      await page.route(api.endpoints.bookmarks.saved, async (route) => {
+        await route.fulfill(deckCatalogueSeed(12, 6));
+      });
+
+      await page.goto(detailsPage);
+
+      const del = page.getByText(/Delete/i);
+      
+      await del.click();
+
+      await page.waitForURL(homePage);
+    });
+
+    test('Deleting from this page redirects to home page when the request fails', async ({ page }) => {
+      await page.route(sessionEndpoint, async (route) => {
+        await route.fulfill(authorizeRequest());
+      });
+
+      await page.route(api.endpoints.decks.delete(1), async (route) => {
+        if (route.request().method() !== 'DELETE') {
+          await route.fallback();
+          return;
+        }
+        
+        await route.abort();
+      });
+
+      await page.route(api.endpoints.bookmarks.saved, async (route) => {
+        await route.fulfill(deckCatalogueSeed(12, 6));
+      });
+
+      await page.goto(detailsPage);
+
+      const del = page.getByText(/Delete/i);
+      
+      await del.click();
+
+      await page.waitForURL(homePage);
+    });
   });
 });
