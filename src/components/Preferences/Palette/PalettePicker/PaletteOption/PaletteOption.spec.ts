7c64a3d1dd91a9e6821ffcb2c845809bd8fcc535
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-05-25 13:27:41 +0300

    add PaletteOption

---
+++ src/components/Preferences/Palette/PalettePicker/PaletteOption/PaletteOption.spec.ts
@@ -0,0 +1,52 @@
+import { describe, expect, it } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/vue';
+
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import { allowedPreferences } from '../../../../../constants/allowedPreferences';
+import PaletteOptionVue from './PaletteOption.vue';
+
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+describe('PaletteOption component', () => {
+  it('Renders and behaves correctly (current theme does not match the option)', async () => {
+    localStorage.setItem('palette', allowedPreferences.palettes[1]);
+    render(PaletteOptionVue, {
+      global: {
+        plugins: [vuetify]
+      },
+      props: {
+        value: allowedPreferences.palettes[0] // 'deepPurple'
+      },
+    });
+
+    const unchecked = document.querySelector(':checked');
+    expect(unchecked).toBeNull();
+
+    const radio = await screen.findByLabelText('Deep purple');
+    await fireEvent.click(radio);
+
+    const checked = document.querySelector(':checked');
+    expect(checked).not.toBeNull();
+  });
+
+  it('Renders correctly (current theme matches the option)', async () => {
+    localStorage.setItem('palette', allowedPreferences.palettes[0]);
+    render(PaletteOptionVue, {
+      global: {
+        plugins: [vuetify]
+      },
+      props: {
+        value: allowedPreferences.palettes[0] // 'deepPurple'
+      },
+    });
+
+    const checked = document.querySelector(':checked');
+    expect(checked).not.toBeNull();
+  });
+});
---
+++ src/components/Preferences/Palette/PalettePicker/PaletteOption/PaletteOption.vue
@@ -0,0 +1,96 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { colors } from '../../../../../constants/colors';
+import type { palette } from '../../../../../types/store/store';
+import { useTheme } from 'vuetify/lib/framework.mjs';
+import { computed } from 'vue';
+
+export interface IPaletteOption {
+  value: palette;
+}
+
+const props = defineProps<IPaletteOption>();
+
+const input = ref<HTMLInputElement>();
+const theme = useTheme();
+
+const matchesTheme = computed(() =>
+  (colors[props.value] === theme.themes.value.light.colors.primary)
+  ||
+  (colors[props.value] === theme.themes.value.dark.colors.primary)
+);
+
+const labels = {
+  deepPurple: 'Deep purple',
+  indigo: 'Indigo',
+  blue: 'Blue',
+  green: 'Green',
+  blueGrey: 'Blue gray',
+  red: 'Red',
+  pink: 'Pink',
+}
+
+function check() {
+  if (input.value && input.value.checked === false) {
+    input.value.checked = true;
+    changePalette();
+  }
+}
+
+function changePalette() {
+  theme.themes.value.light.colors.primary = colors[props.value];
+  theme.themes.value.dark.colors.primary = colors[props.value];
+
+  localStorage.setItem('palette', props.value);
+}
+</script>
+
+<template>
+  <div :class="`option ${matchesTheme ? 'checked' : 'nocheck'}`" @click="check">
+    <label class="color" :for="value" :style="{
+      background: `${colors[value]}`
+    }">
+      {{ labels[value] }}
+    </label>
+    <input :id="value" ref="input" name="palette" type="radio" :class="`radio ${value}`" :value="value" :checked="matchesTheme"
+      @change.prevent="changePalette" />
+    <h2 aria-hidden="true">{{ labels[value] }}</h2>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.option {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
+
+  .color {
+    display: block;
+    width: 80px;
+    height: 30px;
+    cursor: pointer;
+    border-radius: 30px;
+    color: transparent;
+    user-select: none;
+  }
+
+  .radio {
+    visibility: hidden;
+    position: absolute;
+  }
+
+  &.checked .color {
+    border: 2px solid yellow;
+  }
+
+  &.checked h2 {
+    color: #ffc300;
+  }
+
+  h2 {
+    font-size: 12pt;
+  }
+}
+</style>
